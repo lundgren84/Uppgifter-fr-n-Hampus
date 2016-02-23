e1de925bbd82dee883ef0abe93aab904e7abc468
lundgren84 <lundgren84@hotmail.se>
2016-02-23 14:56:20 +0100

    K

---
+++ Exercise9/Exercise9/ERROR.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise9
+{
+    public static class ERROR
+    {
+        public static string CheckSsn()
+        {
+            Console.WriteLine();
+            long ssn = 0;
+            string realSsn = "";
+            bool go = true;
+
+            do
+            {
+                if (!long.TryParse(Console.ReadLine(), out ssn)) { Console.WriteLine("Not a Valid Number\nTyr again. . ."); }
+                else
+                {
+                    realSsn = ssn.ToString();
+                    if (realSsn.Length == 10)
+                    {
+                        go = false;
+                    }
+                    else { Console.WriteLine("You need 10 numbers! Example 8503103845\nTry again. . ."); }
+                }     
+            } while (go);
+
+            return realSsn;
+        }
+        public static string NameCheck()
+        {
+            bool go = true;
+            double test = 0;
+
+            do
+            {
+                if (double.TryParse(Console.ReadLine(), out test)) { Console.WriteLine("You cant put numbers in your name!\nTry again. . ."); }
+                else { go = false; }
+            } while (go);
+
+            string name = test.ToString();
+            return name;
+        }
+        public static string NumberCheckString()
+        {
+            bool go = true;
+            int number = 0;
+            string CheckedNr = string.Empty;
+            do
+            {
+
+                if (int.TryParse(Console.ReadLine(), out number))
+                {
+                    if (number == 0) { Console.WriteLine($"Employee cant have {number} as wage!\nTry again. . .");  }
+                    else { go = false; }
+                }
+                else{ Console.WriteLine("Must write a number!\nTry again. . ."); }
+            } while (go);
+
+            CheckedNr = Convert.ToString(number);
+            return CheckedNr;
+        }
+        public static int NumberCheckInt()
+        {
+            bool go = true;
+            int number = 0;
+            do
+            {
+                if (!int.TryParse(Console.ReadLine(), out number)) { Console.WriteLine("Must write a number!\nTry again. . ."); }
+                else
+                {
+                    if (number == 0) { Console.WriteLine($"Employee cant have {number} as wage!\nTry again. . ."); }
+                    else { go = false; }
+                }
+            } while (go);
+            return number;
+        }
+    }
+}
---
+++ Exercise9/Exercise9/Employee.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise9
+{
+    public class Employee
+    {
+        private string firstName;
+        public string FirstName
+        {
+            get { return this.firstName; }
+            set { this.firstName = value; }
+        }
+        private string lastrName;
+        public string LastName
+        {
+            get { return this.lastrName; }
+            set { this.lastrName = value; }
+        }
+        private string ssn;
+        public string Ssn
+        {
+            get { return this.ssn; }
+            set { this.ssn = value; }
+        }
+
+        private string wage;
+        public string Wage
+        {
+            get { return this.wage; }
+            set { this.wage = value; }
+        }
+
+        public string FullName
+        {
+            get { return FirstName + " " + LastName; }
+        }
+        public Employee(string FirstName, string LastName, string Ssn, string Wage)
+        {
+            this.firstName = FirstName;
+            this.lastrName = LastName;
+            this.ssn = Ssn;
+            this.wage = Wage;
+
+        }
+        public Employee(string Ssn)
+        {
+            this.ssn = Ssn;
+        }
+        public void addEmployee(List<Employee> EmployeeList)
+        {
+            Console.WriteLine("Add Employee\n");
+            Console.WriteLine("FirstName: ");
+            FirstName = Console.ReadLine();
+            Console.WriteLine("LastName: ");
+            LastName = Console.ReadLine();
+            Console.WriteLine("Social security number: ");
+            Ssn = Console.ReadLine();
+            Console.WriteLine("Hourly wage: ");
+            Wage = Console.ReadLine();
+            Employee newEmployee = new Employee(FirstName, LastName, Ssn, Wage);
+            EmployeeList.Add(newEmployee);
+            Console.WriteLine("Employee added!");
+        }
+        public void WriteEmployee(List<Employee> EmployeeList)
+        {
+            Console.Write("Employee name: ");
+            string EmployeeName = Console.ReadLine();
+
+            EmployeeName.ToLower().Replace(" ", "");
+
+            foreach (Employee item in EmployeeList)
+            {
+
+
+                string check = item.FirstName.ToLower() + item.LastName.ToLower();
+
+                if (check == EmployeeName.Replace(" ", ""))
+                {
+                    Console.WriteLine("Employee\n");
+                    Console.WriteLine("Name: {0} {1}", item.FirstName, item.LastName);
+                    Console.WriteLine("Social security number: {0}", item.Ssn);
+                    Console.WriteLine("Hourly wage: {0}\n\n", item.Wage);
+                    return;
+                }
+            }
+            Console.WriteLine("User dont exists!");
+            Console.WriteLine("Press a key. . .");
+            Console.ReadLine();
+        }
+        public void PutSomeEmployeeInList(List<Employee> EmployeeList)
+        {
+            Employee Olle = new Employee("Olle", "Svensson", "20020117-5049", "112");
+            Employee Sven = new Employee("Sven", "Paulander", "19780920-4036", "155");
+            EmployeeList.Add(Olle);
+            EmployeeList.Add(Sven);
+        }
+
+    }
+}
